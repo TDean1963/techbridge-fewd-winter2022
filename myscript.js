a1e8ae0ed10388d12713857a7b53e047ab42f5cb
TDean1963 <terrencemdean12@gmail.com>
2022-02-06 15:31:36 -0500

    Commitnew

--- myscript.js
+++ myscript.js
@@ -1,1 +1,23 @@
+console. log ("inside html file");
+ {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
+  }
+}
 
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "50%";
+  }
+}
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
